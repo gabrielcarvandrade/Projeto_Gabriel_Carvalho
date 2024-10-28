0e9365e834be3d9df9a59ee9eb3b564fe73f8de9
Gabriel Carvalho <gabrielcarvandrade@gmail.com>
2024-10-28 20:40:31 -0300

    Adicionadas classes A e B com métodos e atributos

---
+++ A.java
@@ -0,0 +1,37 @@
+public class A {
+    private int A1;
+    private float A2;
+
+    // Construtor
+    public A(int A1, float A2) {
+        this.A1 = A1;
+        this.A2 = A2;
+    }
+
+    // Getters
+    public int getA1() {
+        return A1;
+    }
+
+    public float getA2() {
+        return A2;
+    }
+
+    // Setters
+    public void setA1(int A1) {
+        this.A1 = A1;
+    }
+
+    public void setA2(float A2) {
+        this.A2 = A2;
+    }
+
+    // Métodos
+    public void MA1() {
+        System.out.println("Método MA1 chamado");
+    }
+
+    public void MA2() {
+        System.out.println("Método MA2 chamado");
+    }
+}
